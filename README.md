73e95e3263e6afc166fb14b53d8d005325ed95da
Evan de Jesus <evan.dejesus@24g.com>
2021-04-05 13:24:46 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Vault AWS Action
---
+++ entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh -l
+
+set -e
+
+if [ -z "$VAULT_TOKEN" ]; then
+	echo "VAULT_TOKEN is not set. Quitting."
+	exit 1
+fi
+
+if [ -z "$VAULT_ROLE" ]; then
+	echo "VAULT_ROLE is not set. Quitting."
+	exit 1
+fi
+
+if [ -z "$VAULT_ADDR" ]; then
+	VAULT_ADDR=https://vault.24g.dev
+fi
+
+vault read -address=$VAULT_ADDR aws/sts/$VAULT_ROLE ttl=30m --format=json >creds.json
+AWS_ACCESS_KEY_ID=$(jq -r '.data.access_key' creds.json)
+AWS_SECRET_ACCESS_KEY=$(jq -r '.data.secret_key' creds.json)
+AWS_SESSION_TOKEN=$(jq -r '.data.security_token' creds.json)
+
+echo "::set-output name=access_key::$AWS_ACCESS_KEY_ID"
+echo "::set-output name=secret_key::$AWS_SECRET_ACCESS_KEY"
+echo "::set-output name=session_token::$AWS_SESSION_TOKEN"
